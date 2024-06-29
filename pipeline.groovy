a6cc5f16928c136a210d3b9752bac6a49edc4041
vidusha dilshan <86625992+pgvda@users.noreply.github.com>
2024-06-29 08:14:59 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -38,9 +38,9 @@ pipeline {
 
         stage('Login to Docker Hub') {
             steps {
-                withCredentials([usernamePassword(credentialsId: 'dockerhub-credential', usernameVariable: 'vidushs', passwordVariable: '19991109_Vidusha')])  {
+                withCredentials([usernamePassword(credentialsId: 'dockerhub-credential', usernameVariable: 'vidushs', passwordVariable: '1999Vidusha')])  {
                     script {
-                        bat "docker login -u ${vidushs} -p ${19991109_Vidusha}"
+                        bat "docker login -u ${vidushs} -p ${1999Vidusha}"
                     }
                 }
             }
